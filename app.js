491f078cb5ee4fc5d408d6ef2a9230bfc1e4cd0c
apavlov3rednet <92976265+apavlov3rednet@users.noreply.github.com>
2025-02-14 10:57:51 +0300

    ready project

--- app.js
+++ app.js
@@ -1,43 +1,28 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+import express from 'express';
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const app = express();
+const PORT = process.env.PORT || 3000;
 
-var app = express();
-
-
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+//use
+/**
+ * C - post
+ * R - get
+ * U - put
+ * D - delete
+ */
+app.get('/', (req, res) => { //requrest, response
+    // console.log(req); //Объект запроса
+    // console.log(res); //Объект ответа
+    res.send("<h2>Это работает</h2>");
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+/**
+ * 1.0.5.1000
+ */
+app.get('/api/', (req, res) => {
+    res.send("<h2>API</h2>");
 });
 
-module.exports = app;
+app.listen(PORT, () => {
+    console.log(`API is listening on port ${PORT}`);
+});
